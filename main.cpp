a890e39827de68bfcc057be76f8a4aec6b7ccd30
Evan Voordeckers <evan.voordeckers@yahoo.com>
2022-11-06 17:48:00 -0500

    Able to collect user input (missing state code) Able to calculate cost (missing state tax and discount %)

--- main.cpp
+++ main.cpp
@@ -4,6 +4,17 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!";
+    int quantity;
+    double cost;
+
+    cout << "Please enter # of items" << endl;
+    cin >> quantity;
+    cout << "You entered " << quantity << " items" << endl;
+    
+    cout << "Please enter cost of items" << endl;
+    cin >> cost;
+    cout << "You entered a cost of " << cost << endl;
+
+    cout << "Total cost: " << cost * quantity << endl;
     return 0;
 }
